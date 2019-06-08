f2309baa0b437f8cfd820ffcae49d0c845c398a1
Douglas Silva Brum <37813801+douglasbrum@users.noreply.github.com>
2019-06-07 23:13:41 -0300

    Update motor.cpp

--- arduino-lib/motor-lib/motor.cpp
+++ arduino-lib/motor-lib/motor.cpp
@@ -6,9 +6,7 @@ void Motor::setMotorDefinitions(int startAngle, int beginLimit, int endLimit){
   servo.attach(counter++);
   limitAngle[0] = beginLimit; 
   limitAngle[1] = endLimit;
-  servo.write(checkRange(angle));
-  //spinTime();
-  //servo.write(targetPos = checkRange(startAngle));
+  servo.write(checkRange(startAngle));
 }
 
 void Motor::goTo(int angle){
@@ -24,30 +22,6 @@ int Motor::checkRange(int targetPos){
  }
  else return targetPos;
 }
-
-/*
-void Motor::goToIn(int angle, unsigned long time){
-  targetPos = checkRange(angle);
-  unsigned long var= (servo.read()-angle > 0 ) ? servo.read()-angle : angle-servo.read();
-  thread.changeTimer(time/var);
-  spinTime();
-}
-boolean Motor::spinTime(){
-  return thread.gotTime(); 
-}
-void Motor::spinMotor(){
-  int aux = servo.read();
-  if(aux < targetPos){
-   aux++;
-  }
-  else if (aux > targetPos){
-   aux--;
-  }
-  servo.write(aux);
-  Serial.println(millis() - thread.logtime);
-  thread.logtime = millis();
-}
-*/
 
 void Motor::returnCount(){
   Serial.println(counter);
