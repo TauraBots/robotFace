0e7ce302f18313f2791d0b6b58e594a5d965f92b
Victor Augusto Kich <victorkich@yahoo.com.br>
2019-10-15 17:10:22 -0300

    Alteracoes no dataController para fazer envios ao arduino

--- src/dataController.py
+++ src/dataController.py
@@ -4,6 +4,7 @@ import rospy
 import time
 import threading
 from std_msgs.msg import (Int16MultiArray, Int16)
+from PyDynamixel import DxlComm, Joint
 
 # Define the output vector
 motors = [50] * 12
@@ -39,26 +40,27 @@ class dataflowEnable():
         #rospy.spin()
 
     def getMouth(self, msg):
+        global motors
         self.data = msg.data
         motors[0] = int(0.3059*self.data[0])
         motors[1] = int(0.3059*self.data[1])
-        print(self.data)
 
     def getEye(self, msg):
+        global motors
         self.data = msg.data
         motors[2] = self.data[0]
         motors[3] = self.data[1]
-        print(self.data)
     
     def getEyelid(self, msg):
+        global motors
         self.data = msg.data
         motors[4] = self.data[0]
         motors[5] = self.data[1]
         motors[6] = self.data[2]
         motors[7] = self.data[3]
-        print(self.data)
 
     def getEyebrown(self, msg):
+        global motors
         self.data = msg.data
         motors[8] = self.data[0]
         motors[9] = self.data[1]
@@ -72,6 +74,22 @@ class dataflowEnable():
 
     def sendArduino(self):
         while(True):
+            global motors
+            '''
+            Joint0.writeValue(motors[0])
+            Joint1.writeValue(motors[1])
+            Joint2.writeValue(motors[2])
+            Joint3.writeValue(motors[3])
+            Joint4.writeValue(motors[4])
+            Joint5.writeValue(motors[5])
+            Joint6.writeValue(motors[6])
+            Joint7.writeValue(motors[7])
+            Joint8.writeValue(motors[8])
+            Joint9.writeValue(motors[9])
+            Joint10.writeValue(motors[10])
+            Joint11.writeValue(motors[11])
+            '''
+            
             time.sleep(0.05)
 
 if __name__ == '__main__':
